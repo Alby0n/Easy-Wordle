bf617a30687bbe65776e8697e438b679d95256f0
Aby Stalin <73681462+Alby0n@users.noreply.github.com>
2023-03-13 12:54:29 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ fetch("https://random-word-api.herokuapp.com/word?number=1&length=5")
     console.error("Error fetching random word:", error);
   });
 
-function checkWord(guess){
-  fetch("https://api.dictionaryapi.dev/api/v2/entries/en_US/" + guess)
+async function checkWord(guess){
+  await fetch("https://api.dictionaryapi.dev/api/v2/entries/en_US/" + guess)
   .then(response => {
     if (response.status === 200) {
       flag=true;
@@ -37,7 +37,7 @@ const guessButton = document.querySelector("button[type='submit']");
 const guessesContainer = document.querySelector(".guesses");
 
 // Function to initialize the word display
-function initializeWordDisplay(word) {
+async function initializeWordDisplay(word) {
   if (flag  == true){
   for (let i = 0; i < word.length; i++) {
     const letterSpan = document.createElement("span");
@@ -47,26 +47,52 @@ function initializeWordDisplay(word) {
   }
 }
 
-guessButton.addEventListener("click", (event) => {
+guessButton.addEventListener("click", async (event) => {
   event.preventDefault();
   
   // Get the user's guess
   const guess = guessInput.value.toLowerCase();
-  checkWord(guess)
+  if (guesses.includes(guess)) {
+    alert("You've already guessed that word!");
+    return;
+  }
+
+  await checkWord(guess).then(() => {
   
   // Check if the guess is valid (5 letters)
   if (guess.length !== 5) {
     alert("Please enter a 5-letter word.");
     return;
   }
- 
+
+  let correct = false;
+  if(flag){
+    correct = true;
+    for (let i = 0; i < wordToGuess.length; i++) {
+      if (guess.includes(wordToGuess[i])) {
+        wordContainer.children[i].textContent = wordToGuess[i];
+      } else {
+        correct = false;
+      }
+    }
+  }
+  // Check if the game is over
+  if (correct) {
+    alert("Congratulations, you guessed the word!");
+  } else if (remainingAttempts === 1) {
+    alert(`Game over. The word was "${wordToGuess}".`);
+  } else if (flag===true) {
+    remainingAttempts--;
+    alert(`Incorrect. You have ${remainingAttempts} attempts remaining.`);
+  }
+  
+  // Clear the input field
+  guessInput.value = "";
   
   
   // Check if the guess has already been made
-  if (guesses.includes(guess)) {
-    alert("You've already guessed that word!");
-    return;
-  }
+  
+});
   
   // Add the guess to the guesses array and update the guesses display
 //   if (flag == true){
@@ -75,26 +101,7 @@ guessButton.addEventListener("click", (event) => {
 // }
   
   // Check if the guess is correct
-  let correct = true;
-  for (let i = 0; i < wordToGuess.length; i++) {
-    if (guess.includes(wordToGuess[i])) {
-      wordContainer.children[i].textContent = wordToGuess[i];
-    } else {
-      correct = false;
-    }
-  }
-  // Check if the game is over
-  if (correct) {
-    alert("Congratulations, you guessed the word!");
-  } else if (remainingAttempts === 1) {
-    alert(`Game over. The word was "${wordToGuess}".`);
-  } else if (flag==true) {
-    remainingAttempts--;
-    alert(`Incorrect. You have ${remainingAttempts} attempts remaining.`);
-  }
   
-  // Clear the input field
-  guessInput.value = "";
 
 
 });
